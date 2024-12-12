6c6b20200ac1307e7c790d6d5d85366b132d930d
DoCongThanhPhuong <tucker.do.developer@gmail.com>
2024-12-12 14:53:49 +0700

    feat: config tailwindcss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,39 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import AppHeader from '@/components/app.header'
+import AppFooter from '@/components/app.footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'HRM System',
-  description: 'HRM System'
+  description: 'HRM System to manage human resources'
 }
 
+/*
+                       _oo0oo_
+                      o8888888o
+                      88" . "88
+                      (| -_- |)
+                      0\  =  /0
+                    ___/`---'\___
+                  .' \\|     |// '.
+                 / \\|||  :  |||// \
+                / _||||| -:- |||||- \
+               |   | \\\  -  /// |   |
+               | \_|  ''\---/''  |_/ |
+               \  .-\__  '-'  ___/-. /
+             ___'. .'  /--.--\  `. .'___
+          ."" '<  `.___\_<|>_/___.' >' "".
+         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+         \  \ `_.   \_ __\ /__ _/   .-` /  /
+     =====`-.____`.___ \_____/___.-`___.-'=====
+                       `=---='
+
+             KEEP EVERYTHING IS SIMPLE!
+     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+       Buddha blesses you with no bugs forever
+ */
 export default function RootLayout({
   children
 }: {
@@ -15,7 +41,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AppHeader />
+        <main>{children}</main>
+        <AppFooter />
+      </body>
     </html>
   )
 }
